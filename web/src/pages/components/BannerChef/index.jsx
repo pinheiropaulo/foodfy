e2426eb33db9f4b2f8936ca9faaa4a7c56056870
Paulo Pinheiro <pauloprap57@gmail.com>
2022-07-09 05:14:55 -0300

    component BannerChef

---
+++ web/src/pages/components/BannerChef/index.jsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+import styles from './style.module.scss';
+
+export default function BannerChef() {
+  return (
+    <div className={styles.chef}>
+      <div className={styles.chef_text}>
+        <h2>As melhores receitas</h2>
+        <p>
+          Aprenda a construir os melhores pratos
+          <br />
+          com receitas criadas por profissionais
+          <br />
+          do mundo inteiro.
+        </p>
+      </div>
+
+      <div className={styles.chef_img}>
+        <Image src="/assets/chef.png" alt="Chef Img" width={215} height={300} />
+      </div>
+    </div>
+  );
+}
--- web/src/pages/index.jsx
+++ web/src/pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import BannerChef from './components/BannerChef';
 import Header from './components/Header';
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
         <title>Foodfy</title>
       </Head>
       <Header />
+      <BannerChef />
     </div>
   );
 }
